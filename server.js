06ac2766f1dd05206f1ce233a419388a89eeac23
Ramaaeln <abdullahramadanelansary@gmail.com>
2025-11-12 18:20:52 +0700

    updated vercel

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running locally on port ${PORT}`));
+}
+
+module.exports = app;
