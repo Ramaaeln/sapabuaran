b957aa2cb6711ea6ab7da45456409b5c98d401b3
Ramaaeln <abdullahramadanelansary@gmail.com>
2025-11-27 16:52:28 +0700

    update

--- src/utils/fcm.js
+++ src/utils/fcm.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('../utils/firebaseAdmin');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
